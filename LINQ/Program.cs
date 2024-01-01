795cb302cbbc75baaefa19d01df6c474d0b7b910
ilyas Osman <ilyaso.fiiidooow@gmail.com>
2023-12-31 18:44:53 -0600

    new

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace LINQ
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
 {
-    public class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        List<string> videoGames = new List<string>
         {
-            Console.WriteLine("Hello, World!");
+            "Grand Theft Auto VI",
+            "FIFA 23",
+            "Super Mario Bros",
+            "FIFA EA Sports 2024",
+            "Grand Theft Auto v",
+            "Call of Duty",
+            "Fortnite",
+            "NBA 2k24"
+        };
+
+        // Order the list of games by the length of the game name
+        var orderedGames = videoGames.OrderBy(game => game.Length).ToList();
+
+        // Print the ordered list
+        Console.WriteLine("Ordered List of Video Games:");
+        foreach (var game in orderedGames)
+        {
+            Console.WriteLine(game);
         }
     }
 }
